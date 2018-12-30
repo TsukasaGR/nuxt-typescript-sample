9d1666f4fd4a03355220f5fcefdbc5eb07349d39
関口 司 <t14159265358979323846@gmail.com>
2018-12-30 13:55:06 +0900

    サンプルコンポーネント追加

---
+++ src/components/Sample.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="sample">
+    <h2>サンプルコンポーネント</h2>
+    <p>{{ propString }}</p>
+    <p>{{ propNumber }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class extends Vue {
+  @Prop()
+  propString!: string
+  @Prop({ type: Number, required: true })
+  propNumber!: number
+
+  sampleVariableString: string = 'サンプル変数(String)'
+  sampleVariableNumber: number = 123
+}
+</script>
+
+<style lang="stylus">
+.sample {
+  border: 2px solid green;
+  padding: 5px;
+}
+</style>
--- src/pages/ts_sample.vue
+++ src/pages/ts_sample.vue
@@ -3,15 +3,24 @@
     <h1>TSサンプルページ</h1>
     <p>{{ sampleVariableString }}</p>
     <p>{{ sampleVariableNumber }}</p>
+    <SampleComponent :prop-string="propString" :prop-number="propNumber"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
-@Component
+import SampleComponent from '~/components/Sample.vue'
+
+@Component({
+  components: {
+    SampleComponent
+  }
+})
 export default class extends Vue {
   sampleVariableString: string = 'サンプル変数(String)'
   sampleVariableNumber: number = 123
+  propString: string = '子に渡す変数'
+  propNumber: number = 345
 }
 </script>
