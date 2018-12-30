235d596f485b247fe7e54a4bdc84e6340360db44
関口司 <t14159265358979323846@gmail.com>
2018-12-30 18:00:04 +0900

    tsconfigの設定を微調整

--- src/modules/sample1.ts
+++ src/modules/sample1.ts
@@ -1,4 +1,6 @@
-import sample2 from '~/modules/sample2'
+// import sample2 from './sample2' // エラー出ない
+// import sample2 from 'src/modules/sample2' // エラー出ない
+import sample2 from '~/modules/sample2' // エラー出る
 
 export default function sample1(): string {
     console.log('sample1.ts passed!')
