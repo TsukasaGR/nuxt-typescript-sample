11d87682b108787ce95de9cc109c1bf12a144119
関口司 <t14159265358979323846@gmail.com>
2018-12-31 14:28:45 +0900

    vuexサンプルページにVuexを利用

---
+++ src/components/organisms/profile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="organisms-profile">
+    <h2>profileコンポーネント</h2>
+    <p>email</p>
+    <p>{{ user.email }}</p>
+    <p>full name</p>
+    <p>{{ fullName }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { State, Getter } from 'vuex-class'
+import { User } from '~/store/organisms/profile/types'
+
+@Component({
+  components: {}
+})
+export default class OrganismsProfile extends Vue {
+  @State('user', { namespace: 'profile' })
+  user!: User
+  @Getter('fullName', { namespace: 'profile' })
+  fullName!: string
+
+  fetch({ store }: { store: any }) {
+    store.dispatch('profile/fetchData')
+  }
+}
+</script>
+
+<style lang="stylus">
+.organisms-profile {
+  border: 2px solid blue;
+  padding: 10px;
+}
+</style>
--- src/pages/vuex_test.vue
+++ src/pages/vuex_test.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <h1>Vuexサンプルページ</h1>
+    <OrganismsProfile/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
+import OrganismsProfile from '~/components/organisms/profile.vue'
+
 @Component({
   components: {
+    OrganismsProfile
   }
 })
-export default class Index extends Vue {
-}
+export default class Index extends Vue {}
 </script>
