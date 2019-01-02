30868020c9732c1890430b77d3bc52fa213367a1
関口司 <t14159265358979323846@gmail.com>
2019-01-02 18:28:44 +0900

    変数には型をつけるように

--- src/store/organisms/profile/actions.ts
+++ src/store/organisms/profile/actions.ts
@@ -1,14 +1,14 @@
 import { ActionTree } from 'vuex'
-import { ProfileState } from './types'
+import { User, ProfileState } from './types'
 import { RootState } from '~/store/types'
 
 export const actions: ActionTree<ProfileState, RootState> = {
   fetchData({ commit }): void {
-    const payload = {
+    const payload: User = {
       firstName: 'firstName',
       lastName: 'lastName',
       email: 'sample@example.com',
-      phone: 11
+      phone: '090-0000-0000'
     }
     commit('setProfile', payload)
   }
--- src/store/organisms/profile/mutations.ts
+++ src/store/organisms/profile/mutations.ts
@@ -3,9 +3,6 @@ import { ProfileState, User } from './types'
 
 export const mutations: MutationTree<ProfileState> = {
   setProfile(state, payload: User) {
-    console.log('mutation', typeof payload)
-    const test: User = payload
-    console.log(typeof <User>test)
     state.user = payload
   }
 }
